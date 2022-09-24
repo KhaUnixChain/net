6e083a51a340d8fe016c425dd90439fcf5e42af2
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-09-24 16:04:41 +0700

    upload hide code time

--- src/main/resources/static/dist/js/all_feature.js
+++ src/main/resources/static/dist/js/all_feature.js
@@ -66,81 +66,81 @@ function payment(number) {
 }
 
 
-setInterval(function () {
-    if (hours_food == 0 && minute_food == 0 && second_food == 1) {
-        document.getElementById("discount_food").innerHTML = "00 : 00 : 00";
-        document.getElementById("discount_100").innerHTML = "00 : 00 : 00";
-    } else {
-        second_food--;
-        second_100--;
-        if (second_food == 0) {
-            minute_food--;
-            second_food = 60;
+// setInterval(function () {
+//     if (hours_food == 0 && minute_food == 0 && second_food == 1) {
+//         document.getElementById("discount_food").innerHTML = "00 : 00 : 00";
+//         document.getElementById("discount_100").innerHTML = "00 : 00 : 00";
+//     } else {
+//         second_food--;
+//         second_100--;
+//         if (second_food == 0) {
+//             minute_food--;
+//             second_food = 60;
 
-            if (minute_food == 0) {
-                hours_food--;
-                minute_food = 59;
+//             if (minute_food == 0) {
+//                 hours_food--;
+//                 minute_food = 59;
 
-                if (hours_food == 0) {
-                    hours_food = hours_food;
-                }
-            }
-        }
+//                 if (hours_food == 0) {
+//                     hours_food = hours_food;
+//                 }
+//             }
+//         }
 
-        if (second_100 == 0) {
-            minute_100--;
-            second_100 = 60;
+//         if (second_100 == 0) {
+//             minute_100--;
+//             second_100 = 60;
 
-            if (minute_100 == 0) {
-                hours_100--;
-                minute_100 = 59;
+//             if (minute_100 == 0) {
+//                 hours_100--;
+//                 minute_100 = 59;
 
-                if (hours_100 == 0) {
-                    hours_100 = hours_100;
-                }
-            }
-        }
+//                 if (hours_100 == 0) {
+//                     hours_100 = hours_100;
+//                 }
+//             }
+//         }
 
-        if (hours_food.toString().length == 1) {
-            hours_food = "0" + hours_food;
-        }
+//         if (hours_food.toString().length == 1) {
+//             hours_food = "0" + hours_food;
+//         }
 
-        if (minute_food.toString().length == 1) {
-            minute_food = "0" + minute_food;
-        }
+//         if (minute_food.toString().length == 1) {
+//             minute_food = "0" + minute_food;
+//         }
 
-        if (second_food.toString().length == 1) {
-            second_food = "0" + second_food;
-        }
+//         if (second_food.toString().length == 1) {
+//             second_food = "0" + second_food;
+//         }
 
-        if (hours_food==0 && minute_food == 0 && second_food == 0) {
-            clearInterval(this);
-        }
+//         if (hours_food==0 && minute_food == 0 && second_food == 0) {
+//             clearInterval(this);
+//         }
 
-        curent_second_food += 1;
-        percent_food = Math.round(curent_second_food * 100 / sum_second_food);
-        document.getElementById("discount_food").innerHTML = hours_food + " : " + minute_food + " : " + second_food;
-        document.getElementById("progress-bar-food").style.width = 50 - percent_food + "%";
-
-
-        curent_second_100 += 1;
-        percent_100 = Math.round(curent_second_100 * 100 / sum_second_100);
-        document.getElementById("discount_100").innerHTML = hours_100 + " : " + minute_100 + " : " + second_100;
-        document.getElementById("progress-bar-100").style.width =25 - percent_100 + "%";
-    };
-}, 1000);
+//         curent_second_food += 1;
+//         percent_food = Math.round(curent_second_food * 100 / sum_second_food);
+//         document.getElementById("discount_food").innerHTML = hours_food + " : " + minute_food + " : " + second_food;
+//         document.getElementById("progress-bar-food").style.width = 50 - percent_food + "%";
 
 
-setInterval(function() {
-    var thoigian = new Date();
-    var am_pm = "";
-    var hour = thoigian.getHours();
-    if (hour > 12) {
-        am_pm = " PM";
-        hour = hour - 12;
-    }
-    else{
-        am_pm = " AM";
-    }
-    document.getElementById("timeBlock").innerHTML = (hour < 10 ? "0" + hour : hour) + " : " + (thoigian.getMinutes() < 10 ? "0" + thoigian.getMinutes() : thoigian.getMinutes()) + " : " + (thoigian.getSeconds() < 10 ? "0" + thoigian.getSeconds() : thoigian.getSeconds()) + am_pm;
-}, 1000);
+//         curent_second_100 += 1;
+//         percent_100 = Math.round(curent_second_100 * 100 / sum_second_100);
+//         document.getElementById("discount_100").innerHTML = hours_100 + " : " + minute_100 + " : " + second_100;
+//         document.getElementById("progress-bar-100").style.width =25 - percent_100 + "%";
+//     };
+// }, 1000);
+
+
+// setInterval(function() {
+//     var thoigian = new Date();
+//     var am_pm = "";
+//     var hour = thoigian.getHours();
+//     if (hour > 12) {
+//         am_pm = " PM";
+//         hour = hour - 12;
+//     }
+//     else{
+//         am_pm = " AM";
+//     }
+//     document.getElementById("timeBlock").innerHTML = (hour < 10 ? "0" + hour : hour) + " : " + (thoigian.getMinutes() < 10 ? "0" + thoigian.getMinutes() : thoigian.getMinutes()) + " : " + (thoigian.getSeconds() < 10 ? "0" + thoigian.getSeconds() : thoigian.getSeconds()) + am_pm;
+// }, 1000);
