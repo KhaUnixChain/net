b5d80c7f21511118c50158f8d645f0ee57ccbd7a
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-02 00:40:43 +0700

    Add requestMapping login another link

--- src/main/java/com/fastshop/net/controller/auth/AuthController.java
+++ src/main/java/com/fastshop/net/controller/auth/AuthController.java
@@ -49,6 +49,26 @@ public class AuthController {
     }
 
 
+
+    // this is function access login when user login from form
+    @RequestMapping("/auth/company/login")
+    public String authLoginStaff(@Validated @ModelAttribute("dtoLogin") DtoLogin dtoLogin, Errors errors) {
+        if (errors.hasErrors()) {
+            return "employee/login_staff_admin";
+        }
+        else {
+            try {
+                Account account = accountService.findByUsernameOrEmailAndPassword(dtoLogin.getUsernameOrEmail().trim(), dtoLogin.getUsernameOrEmail().trim(), dtoLogin.getPassword().trim());
+                Authority auth = authorityService.findByAccount(account);
+                cookie.add("username", auth.getAccount().getUsername(), 30*24*60*60);
+                return auth.getRole().getId().equals("ADMIN") ? "redirect:/admin/home" : "redirect:/staff/home";
+            } catch (Exception e) {
+                return "error/error-404";
+            }
+        }
+    }
+
+
     // this is function access signup
     @RequestMapping("/auth/signup")
     public String signup(@Validated @ModelAttribute("account") Account account, Errors errors) {
