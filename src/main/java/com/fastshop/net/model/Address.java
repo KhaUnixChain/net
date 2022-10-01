5367a4f41a4237b429beab08cd4479456a05fc85
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-01 16:06:15 +0700

    This is address model

--- src/main/java/com/fastshop/net/model/Address.java
+++ src/main/java/com/fastshop/net/model/Address.java
@@ -1,17 +1,14 @@
 package com.fastshop.net.model;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -22,14 +19,13 @@ import lombok.Data;
 public class Address implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Integer id;
+    Long id;
 
     @Column(name = "place", columnDefinition = "nvarchar(200)", nullable = false)
     String place;
 
     Boolean choose;
 
-    @JsonIgnore
-    @OneToMany(mappedBy = "address")
-    List<Order> orders;
+    @Column(name = "username", columnDefinition = "varchar(15)", nullable = false)
+    String username;
 }
