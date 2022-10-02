73a32ef0ba6493878137425a29c4b6e8899ea4ca
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-03 01:22:46 +0700

    Access for all of address service repository

--- src/main/java/com/fastshop/net/service/AddressService.java
+++ src/main/java/com/fastshop/net/service/AddressService.java
@@ -2,7 +2,12 @@ package com.fastshop.net.service;
 
 import java.util.List;
 
+import com.fastshop.net.model.Address;
+
 public interface AddressService {
-    List<String> findAllAddressByAccount(String username);
+    void save(Address address);
+    void update(String username, Long id);
+    List<Address> findAllAddressByAccount(String username);
     String findByAccountWithChooseIsTrue(String username);
+    Address findByUsernameAndId(String username, Long id);
 }
