a9c100567f11b6031d4b3dfc844bff4448d8e9c5
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-02 15:56:49 +0700

    Design interface CategoryService

--- src/main/java/com/fastshop/net/service/CategoryDetailService.java
+++ src/main/java/com/fastshop/net/service/CategoryDetailService.java
@@ -6,6 +6,6 @@ import com.fastshop.net.model.Category;
 import com.fastshop.net.model.CategoryDetail;
 
 public interface CategoryDetailService {
-    
+    void save(CategoryDetail categoryDetail);
     List<CategoryDetail> findByCategory(Category category);
 }
