76f83fcee897e2cbfed87d1d24c3110e0a8f6fcc
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-09 00:20:46 +0700

    Product service impl which have condition to search

--- src/main/java/com/fastshop/net/service/impl/ProductServiceImpl.java
+++ src/main/java/com/fastshop/net/service/impl/ProductServiceImpl.java
@@ -2,7 +2,6 @@ package com.fastshop.net.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
