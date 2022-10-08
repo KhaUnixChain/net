2cc51d892d033fe836ea39432bb1ea9415e22178
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-08 11:01:04 +0700

    Add enable field in account model

--- src/main/java/com/fastshop/net/model/Account.java
+++ src/main/java/com/fastshop/net/model/Account.java
@@ -53,6 +53,8 @@ public class Account  implements Serializable{
 	@Pattern(regexp = "^(0|\\+84)(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})$", message = "(*) Phone is invalid of international number")
 	@Column(name = "phone", columnDefinition = "varchar(15)", nullable = false, unique = true)
 	private String phone;
+
+	Boolean enable;
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "account")
