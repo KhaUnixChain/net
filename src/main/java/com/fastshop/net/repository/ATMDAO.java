e90407ef924544cdcd1fceb8e3144c169ba64cc4
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-10-23 14:36:35 +0700

    model ATM

---
+++ src/main/java/com/fastshop/net/repository/ATMDAO.java
@@ -0,0 +1,11 @@
+package com.fastshop.net.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fastshop.net.model.ATM;
+
+@Repository
+public interface ATMDAO extends JpaRepository<ATM, Integer> {
+    
+}
