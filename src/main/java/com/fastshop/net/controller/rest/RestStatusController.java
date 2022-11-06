bc285c7e10d872bf9131b5177b608c0cd239bb73
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-11-06 12:31:22 +0700

    extra @restcontroller

--- src/main/java/com/fastshop/net/controller/rest/RestStatusController.java
+++ src/main/java/com/fastshop/net/controller/rest/RestStatusController.java
@@ -3,10 +3,12 @@ package com.fastshop.net.controller.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fastshop.net.model.Status;
 import com.fastshop.net.service.StatusService;
 
+@RestController
 public class RestStatusController {
     @Autowired
     StatusService statusService;
