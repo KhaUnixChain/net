91f6ab74e5d290b06b7dbd07263aa5c2af39f4e8
NguyenNgocKha-99-IT <khann.it.159@gmail.com>
2022-11-13 11:25:32 +0700

    create function get number code vourcher

--- src/main/java/com/fastshop/net/utils/CreateCode.java
+++ src/main/java/com/fastshop/net/utils/CreateCode.java
@@ -1,5 +1,8 @@
 package com.fastshop.net.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreateCode {
     private static String code = "";
     private static int[] numberArr = new int[]{0,1,2,3,4,5,6,7,8,9};
@@ -11,5 +14,13 @@ public class CreateCode {
             code += (i%5==0) ? "-" : numberArr[random];
         }
         return code;
+    }
+
+    public static List<String> getToNumber(int number) {
+        List<String> codes = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            codes.add(getOne());
+        }
+        return codes;
     }
 }
